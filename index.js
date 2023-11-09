5601f2e8cd220de17ad9001e59d98d79be51760c
n1xsoph1c <pttrafiahmed@gmail.com>
2023-11-09 19:46:41 +0600

    INIT

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const app = express()
+const fs = require("fs")
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/src/index.html")
+})
+
+app.get("/video", (req, res) => {
+    const range = req.headers.range;
+
+    if (!range) {
+        res.status(400).send("Requires Range Header");
+        return;
+    }
+
+    const videoPath = "Test-2.mp4";
+    const videoSize = fs.statSync(videoPath).size;
+
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+
+    const contentLength = end - start + 1
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength, 
+        "Content-Type": "video/mp4"
+    }
+
+    res.writeHead(206, headers)
+
+    const videoStream = fs.createReadStream(videoPath, {start, end})
+    videoStream.pipe(res)
+})
+
+
+app.listen(8000, () => {
+    console.log("listening on http://localhost:8000/")
+})
